7d853e9463e21a711f16c6e09741f38f46d9954f
Chandrashekhar kumar <pachandrashekhar550@gmail.com>
2026-04-23 21:28:33 +0530

    Add base URL for reset password

--- src/main/java/com/chandrashekhar/clothingstore/service/EmailService.java
+++ src/main/java/com/chandrashekhar/clothingstore/service/EmailService.java
@@ -1,28 +1,31 @@
 package com.chandrashekhar.clothingstore.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
-	
-	private final JavaMailSender mailSender;
-	
-	public EmailService(JavaMailSender mailSender) {
-		this.mailSender =mailSender; 
-	}
-	
-	public void sendResetPasswordEmail(String toEmail, String token) {
-		
-		String resetLink = "http://localhost:8080/reset-password/" + token;
-		
-		SimpleMailMessage message = new SimpleMailMessage();
-		
-		message.setTo(toEmail);
-		message.setSubject("Password Reset Request");
-		message.setText("Click this link to reset your password:\n" +  resetLink);
-		
-		mailSender.send(message);
-	}
+
+    private final JavaMailSender mailSender;
+
+    @Value("${app.base-url}")
+    private String baseUrl;   // 👈 dynamic URL
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendResetPasswordEmail(String toEmail, String token) {
+
+        String resetLink = baseUrl + "/reset-password/" + token;
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject("Password Reset Request");
+        message.setText("Click this link to reset your password:\n" + resetLink);
+
+        mailSender.send(message);
+    }
 }
